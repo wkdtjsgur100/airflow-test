5089d36f7361ba707effd88ee32d65a465a53e09
wkdtjsgur100 <sunhyuk@rapportlabs.kr>
2022-01-26 02:26:58 +0900

    t

--- test2.py
+++ test2.py
@@ -1,45 +1,45 @@
-from datetime import datetime, timedelta
-
-from kubernetes.client import models as k8s
-from airflow.models import DAG, Variable
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.kubernetes.secret import Secret
-from airflow.kubernetes.pod import Resources
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
-
-dag_id = 'kubernetes-dag'
-
-task_default_args = {
-    'owner': 'bomwo',
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2020, 11, 21),
-    'depends_on_past': False,
-    'email': ['bomwo25@mgmail.com'],
-    'email_on_retry': False,
-    'email_on_failure': True,
-    'execution_timeout': timedelta(hours=1)
-}
-
-dag = DAG(
-    dag_id=dag_id,
-    description='kubernetes pod operator',
-    default_args=task_default_args,
-    schedule_interval='5 16 * * *',
-    max_active_runs=1
-)
-
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    do_xcom_push=True,
-    dag=dag
-)
-
-k.dry_run()
+# from datetime import datetime, timedelta
+# 
+# from kubernetes.client import models as k8s
+# from airflow.models import DAG, Variable
+# from airflow.operators.dummy_operator import DummyOperator
+# from airflow.kubernetes.secret import Secret
+# from airflow.kubernetes.pod import Resources
+# from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+#     KubernetesPodOperator,
+# )
+# 
+# dag_id = 'kubernetes-dag'
+# 
+# task_default_args = {
+#     'owner': 'bomwo',
+#     'retries': 3,
+#     'retry_delay': timedelta(minutes=5),
+#     'start_date': datetime(2020, 11, 21),
+#     'depends_on_past': False,
+#     'email': ['bomwo25@mgmail.com'],
+#     'email_on_retry': False,
+#     'email_on_failure': True,
+#     'execution_timeout': timedelta(hours=1)
+# }
+# 
+# dag = DAG(
+#     dag_id=dag_id,
+#     description='kubernetes pod operator',
+#     default_args=task_default_args,
+#     schedule_interval='5 16 * * *',
+#     max_active_runs=1
+# )
+# 
+# k = KubernetesPodOperator(
+#     name="hello-dry-run",
+#     image="debian",
+#     cmds=["bash", "-cx"],
+#     arguments=["echo", "10"],
+#     labels={"foo": "bar"},
+#     task_id="dry_run_demo",
+#     do_xcom_push=True,
+#     dag=dag
+# )
+# 
+# k.dry_run()
